0182830e450996661ee82276edd926f06b64b8eb
Dorna <94217830+imani2004@users.noreply.github.com>
2022-06-24 18:03:56 +0300

    Update settings.py

--- I4G013166NIM/I4G013166NIM/settings.py
+++ I4G013166NIM/I4G013166NIM/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3)5m!jtw7!z6m3&2ns3$@bubcl9q82!d&$(7(=&*qgi178&w5a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
